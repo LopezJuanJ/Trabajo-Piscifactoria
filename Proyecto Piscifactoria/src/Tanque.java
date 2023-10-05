18065d9dafbddca94de1240e2dccf4387a83c339
matipebu <matipebu@hotmail.com>
2023-10-05 18:20:43 +0200

    Metodo vacio Tanque

--- Proyecto Piscifactoria/src/Tanque.java
+++ Proyecto Piscifactoria/src/Tanque.java
@@ -1,3 +1,20 @@
 public class Tanque {
+    public void showStatus(){
+ 
+      }
+
+    
+    public void showFishStatus(){
+ 
+    }
+    
+    public void showCapacity(){
+ 
+    }
+    
+    public void nextDay(){
+ 
+    }
+    
     
 }
