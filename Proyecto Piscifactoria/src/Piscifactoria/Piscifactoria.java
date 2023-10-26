5e2fd63b88c1b8e3d69d360f3781c127f435a8cc
Juan Javier López <juanjavierlopezgarrido@gmail.com>
2023-10-26 23:10:07 +0200

    Cambios Tanq

--- Proyecto Piscifactoria/src/Piscifactoria/Piscifactoria.java
+++ Proyecto Piscifactoria/src/Piscifactoria/Piscifactoria.java
@@ -1,10 +1,35 @@
 package Piscifactoria;
+
+import java.util.ArrayList;
+
+import Peces.IRio;
+import Peces.Pez;
+import Tanque.Tanque;
+
 public  class Piscifactoria {
     protected int comidaActual;
     protected int comidaMaxima;
+    protected int espacioMaximo;
     protected String nombre;
     
+   
+    protected String tipo;
+    public ArrayList<Tanque<? extends Pez>> tanques;
     
+     public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public int obtenerCapacidadMaximaPis(){
+        if(this instanceof IRio){
+        return  25;
+    }else{
+        return 100;
+    }
+    }
     public Piscifactoria(String nombre) {
         
         this.nombre = nombre;
@@ -23,13 +48,29 @@ public  class Piscifactoria {
     }
 
     public void showStatus(){
-    
+    System.out.println("=============== nombre ===============");
+    System.out.println("Tanques: ");
+    System.out.println("Ocupación: peces / max (x%)");
+    System.out.println("Peces vivos: vivos / total (x%)");
+    System.out.println("Peces alimentados: alimentados / vivos (x%)");
+    System.out.println("Hembras / Machos: H/M");
+    System.out.println("Fértiles: fértiles / vivos");
+    System.out.println("Almacén de comida: actual / max (x%)");
     }
     public void showTankStatus(){
+    for (Tanque tanque: tanques){
+        
+        String nombreTanque = tanque.getNombre();
+        Tanque<? extends Pez> tnq = new Tanque<Pez>(nombreTanque, obtenerCapacidadMaximaPis());
+        tnq.showStatus();
+    }
+     
     
+
     }
     public void showFishStatus(){
-    
+        Tanque<? extends Pez> tnq = new Tanque<Pez>(nombre, comidaActual);
+        tnq.showFishStatus();
     }
     public void showCapacity(){
     
